bf67a2e0ea3436fa11436ec46030786f994456c4
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2025-10-10 12:13:47 -0500

    :sparkles: Set up Supabase and auth module

---
+++ src/entities/index.ts
@@ -0,0 +1,10 @@
+export { UserProfile } from './user-profile.entity';
+export { Group } from './group.entity';
+export { GroupMember } from './group-member.entity';
+export {
+  IndividualCup,
+  CupStatus,
+  EliminationRound,
+} from './individual-cup.entity';
+export { Match, WinnerTeam } from './match.entity';
+export { MatchPlayer, Team } from './match-player.entity';
